hamclub/MMDVM-IQ-BG5HHP

commit 7d24cc01b94eaf5f5db4a159dfa7d8812f32aa4b

Switch the SX1255 transmitter off properly.

IO.cpp
@@ -243,7 +243,7 @@ void CIO::stop()
   m_rxStream = nullptr;
   m_txStream = nullptr;
   m_device   = nullptr;
-  
+
   m_soapyInit = false;
 }
 
@@ -284,7 +284,7 @@ void CIO::process()
 
   void *buffs[1] = {(void*)m_buffer.data()};
   long long timeNs = 0LL;
-  
+
   if (m_soapyInit) {
     int flags = 0;
     int ret = m_device->readStream(m_rxStream, buffs, m_buffer.size(), flags, timeNs);
@@ -320,6 +320,7 @@ void CIO::process()
   if (!m_txBuffer.hasData() && m_tx) {
     m_tx = false;
     LogMessage("TX OFF");
+    m_device->setAntenna(SOAPY_SDR_TX, TX_CHANNEL, "NONE");
   }
 
   while (m_rxBuffer.dataSize() >= RX_BLOCK_SIZE) {
@@ -547,7 +548,7 @@ void CIO::processIQBlock()
   }
 
   // Insert a channel filter here
-  
+
   m_fdudc->process(m_buffer, [this](std::complex<float> rxIQSample) {
     std::complex<float> txIQSample = {0.0F, 0.0F};
     TXSample txSample = {0, MARK_NONE};
@@ -562,7 +563,7 @@ void CIO::processIQBlock()
     // Demodulate RX
     float d = std::arg(rxIQSample * std::conj(m_prevRXIQSample));
     m_prevRXIQSample = rxIQSample;
- 
+
     // Scale -pi...pi to -4096...4096
     d *= 4096.0F / M_PI;
 
@@ -591,6 +592,7 @@ void CIO::write(MMDVM_STATE mode, const q15_t* samples, uint16_t length, const u
   if (!m_tx) {
       m_tx = true;
       LogMessage("TX ON");
+      m_device->setAntenna(SOAPY_SDR_TX, TX_CHANNEL, "TX");
   }
 
   q15_t txLevel;
@@ -737,8 +739,8 @@ uint8_t CIO::setParameters()
       m_device->setGain(SOAPY_SDR_RX, RX_CHANNEL, 30.0);
       m_device->setGain(SOAPY_SDR_TX, TX_CHANNEL, 60.0);
     } else {
-      m_device->setAntenna(SOAPY_SDR_RX, RX_CHANNEL, "LNAL");
-      m_device->setAntenna(SOAPY_SDR_TX, TX_CHANNEL, "BAND1");
+      m_device->setAntenna(SOAPY_SDR_RX, RX_CHANNEL, "RX");
+      m_device->setAntenna(SOAPY_SDR_TX, TX_CHANNEL, "NONE");
 
       m_device->setGain(SOAPY_SDR_RX, RX_CHANNEL, 50.0);
       m_device->setGain(SOAPY_SDR_TX, TX_CHANNEL, 30.0);

DelayBuffer.h
@@ -27,32 +27,32 @@ public:
     m_length(length),
     m_index(0)
     {
-        for (size_t i = 0; i < length; i++) {
+        for (size_t i = 0; i < length; i++)
             m_buffer[i] = initial_value;
-        }
     }
 
-    ~CDelayBuffer() {
+    ~CDelayBuffer()
+    {
         delete[] m_buffer;
     }
 
     TDATATYPE process(TDATATYPE input)
     {
-        if (m_length == 0) {
+        if (m_length == 0)
             return input;
-        }
+
         auto delayed = m_buffer[m_index];
         m_buffer[m_index] = input;
-        if (++m_index >= m_length) {
+        if (++m_index >= m_length)
             m_index = 0;
-        }
+
         return delayed;
     }
 
 private:
     TDATATYPE *m_buffer;
-    size_t m_length;
-    size_t m_index;
+    size_t     m_length;
+    size_t     m_index;
 };
 
 #endif

Version.h
@@ -19,6 +19,6 @@
 #if !defined(VERSION_H)
 #define  VERSION_H
 
-#define VERSION "20260513"
+#define VERSION "20260528"
 
 #endif

commit 8a699e2dea8fc740e3ff55cd328359f0b605bfd2

Some fixes to support newer LimeSDR devices like the LimeSDR-mini
Also fixes issue introduced in commit 7d24cc01 which doesn't
set the antenna back correctly for the Pluto and Lime devices

Conf.cpp
@@ -149,6 +149,10 @@ bool CConf::read()
 				m_modemType = value;
 			else if (::strcmp(key, "URI") == 0)
 				m_modemURI = value;
+			else if (::strcmp(key, "RxGain") == 0)
+				m_rxGain = (unsigned int)::atoi(value);
+			else if (::strcmp(key, "TxGain") == 0)
+				m_txGain = (unsigned int)::atoi(value);
 		} else if (section == SECTION::MMDVM_HOST) {
 			if (::strcmp(key, "HostAddress") == 0)
 				m_networkHostAddress = value;
@@ -227,6 +231,18 @@ std::string CConf::getModemURI() const
 {
 	return m_modemURI;
 }
+
+unsigned int CConf::getRxGain() const
+{
+	return m_txGain;
+}
+
+unsigned int CConf::getTxGain() const
+{
+	return m_txGain;
+}
+
+
 
 bool CConf::getModemTrace() const
 {

Conf.h
@@ -49,6 +49,8 @@ public:
 	// The Modem section
 	std::string  getModemType() const;
 	std::string  getModemURI() const;
+	unsigned int getRxGain() const;
+	unsigned int getTxGain() const;
 	bool         getModemTrace() const;
 
 	// The MMDVMHost section
@@ -66,16 +68,18 @@ private:
 	unsigned int m_logDisplayLevel;
 	unsigned int m_logMQTTLevel;
 
-    std::string  m_mqttHost;
-    unsigned short m_mqttPort;
-    unsigned int m_mqttKeepalive;
-    std::string  m_mqttName;
-    bool         m_mqttAuthEnabled;
-    std::string  m_mqttUsername;
-    std::string  m_mqttPassword;
+	std::string  m_mqttHost;
+	unsigned short m_mqttPort;
+	unsigned int m_mqttKeepalive;
+	std::string  m_mqttName;
+	bool         m_mqttAuthEnabled;
+	std::string  m_mqttUsername;
+	std::string  m_mqttPassword;
 
-    std::string  m_modemType;
-    std::string  m_modemURI;
+	std::string  m_modemType;
+	std::string  m_modemURI;
+	unsigned int m_rxGain;
+	unsigned int m_txGain;
 	bool         m_modemTrace;
 
 	std::string  m_networkHostAddress;

IO.cpp
@@ -125,6 +125,8 @@ m_power(0.0F),
 m_txFreq(0U),
 m_rxFreq(0U),
 m_pocsagFreq(0U),
+m_rxGain(50.0F),
+m_txGain(30.0F),
 m_soapyTXFreq(0.0),
 m_soapyRXFreq(0.0),
 m_soapyPocsagFreq(0.0),
@@ -320,7 +322,14 @@ void CIO::process()
   if (!m_txBuffer.hasData() && m_tx) {
     m_tx = false;
     LogMessage("TX OFF");
-    m_device->setAntenna(SOAPY_SDR_TX, TX_CHANNEL, "NONE");
+    if (m_soapyDeviceType.compare("plutosdr") == 0 || m_soapyDeviceType.compare("pluto") == 0
+      || m_soapyDeviceType.compare("limesdr") == 0 || m_soapyDeviceType.compare("lime") == 0
+      || m_soapyDeviceType.compare("limemini") == 0 || m_soapyDeviceType.compare("lime-mini") == 0) {
+      m_device->setGain(SOAPY_SDR_TX, TX_CHANNEL, 1.0);
+    }
+    else {
+      m_device->setAntenna(SOAPY_SDR_TX, TX_CHANNEL, "NONE");
+    }
   }
 
   while (m_rxBuffer.dataSize() >= RX_BLOCK_SIZE) {
@@ -592,7 +601,14 @@ void CIO::write(MMDVM_STATE mode, const q15_t* samples, uint16_t length, const u
   if (!m_tx) {
       m_tx = true;
       LogMessage("TX ON");
-      m_device->setAntenna(SOAPY_SDR_TX, TX_CHANNEL, "TX");
+      if (m_soapyDeviceType.compare("plutosdr") == 0 || m_soapyDeviceType.compare("pluto") == 0
+        || m_soapyDeviceType.compare("limesdr") == 0 || m_soapyDeviceType.compare("lime") == 0
+        || m_soapyDeviceType.compare("limemini") == 0 || m_soapyDeviceType.compare("lime-mini") == 0) {
+        m_device->setGain(SOAPY_SDR_TX, TX_CHANNEL, m_txGain);
+      }
+      else {
+        m_device->setAntenna(SOAPY_SDR_TX, TX_CHANNEL, "TX");
+      }
   }
 
   q15_t txLevel;
@@ -638,7 +654,12 @@ void CIO::setMode(MMDVM_STATE state)
 uint8_t CIO::setParameters()
 {
   stop();
-
+  if(m_trace) {
+    SoapySDR::setLogLevel(SOAPY_SDR_DEBUG);
+  }
+  else {
+    SoapySDR::setLogLevel(SOAPY_SDR_INFO);
+  }
   SoapySDR::Kwargs devArgs;
   SoapySDR::Kwargs rxArgs;
   SoapySDR::Kwargs txArgs;
@@ -680,6 +701,22 @@ uint8_t CIO::setParameters()
     m_timestamped = true;
 
     LogMessage("Using Lime SDR driver uri %s", uri);
+  } else if (m_soapyDeviceType.compare("limemini") == 0 || m_soapyDeviceType.compare("limenet-micro") == 0) {
+    const char* uri = m_soapyDeviceURI.empty() ? LIME_DEFAULT_URI : m_soapyDeviceURI.c_str();
+
+    resampNum = 2U;
+    resampDen = 50U;
+    blockSize = 2048U;
+    iqHWDelay = 50U;
+
+    devArgs["driver"] = "lime";
+    rxArgs["uri"]     = uri;
+    rxArgs["latency"] = "0";
+    txArgs["latency"] = "0";
+
+    m_timestamped = true;
+
+    LogMessage("Using LimeSDR-mini driver uri %s", uri);
   } else if (m_soapyDeviceType.compare("mucell") == 0) {
     resampNum = 4U;
     resampDen = 25U;
@@ -741,20 +778,26 @@ uint8_t CIO::setParameters()
       m_device->setAntenna(SOAPY_SDR_RX, RX_CHANNEL, "A_BALANCED");
       m_device->setAntenna(SOAPY_SDR_TX, TX_CHANNEL, "A");
 
-      m_device->setGain(SOAPY_SDR_RX, RX_CHANNEL, 30.0);
-      m_device->setGain(SOAPY_SDR_TX, TX_CHANNEL, 89.75);
+      m_device->setGain(SOAPY_SDR_RX, RX_CHANNEL, m_rxGain);
+      m_device->setGain(SOAPY_SDR_TX, TX_CHANNEL, m_txGain);
     } else if (m_soapyDeviceType.compare("limesdr") == 0 || m_soapyDeviceType.compare("lime") == 0) {
       m_device->setAntenna(SOAPY_SDR_RX, RX_CHANNEL, "LNAH");
       m_device->setAntenna(SOAPY_SDR_TX, TX_CHANNEL, "BAND1");
 
-      m_device->setGain(SOAPY_SDR_RX, RX_CHANNEL, 30.0);
-      m_device->setGain(SOAPY_SDR_TX, TX_CHANNEL, 60.0);
+      m_device->setGain(SOAPY_SDR_RX, RX_CHANNEL, m_rxGain);
+      m_device->setGain(SOAPY_SDR_TX, TX_CHANNEL, m_txGain);
+    } else if (m_soapyDeviceType.compare("limemini") == 0 || m_soapyDeviceType.compare("lime-mini") == 0) {
+      m_device->setAntenna(SOAPY_SDR_RX, RX_CHANNEL, "Auto");
+      m_device->setAntenna(SOAPY_SDR_TX, TX_CHANNEL, "Auto");
+
+      m_device->setGain(SOAPY_SDR_RX, RX_CHANNEL, m_rxGain);
+      m_device->setGain(SOAPY_SDR_TX, TX_CHANNEL, m_txGain);
     } else {
       m_device->setAntenna(SOAPY_SDR_RX, RX_CHANNEL, "RX");
       m_device->setAntenna(SOAPY_SDR_TX, TX_CHANNEL, "NONE");
 
-      m_device->setGain(SOAPY_SDR_RX, RX_CHANNEL, 50.0);
-      m_device->setGain(SOAPY_SDR_TX, TX_CHANNEL, 30.0);
+      m_device->setGain(SOAPY_SDR_RX, RX_CHANNEL, m_rxGain);
+      m_device->setGain(SOAPY_SDR_TX, TX_CHANNEL, m_txGain);
     }
 
     m_rxStream = m_device->setupStream(SOAPY_SDR_RX, "CF32", {RX_CHANNEL}, rxArgs);
@@ -774,10 +817,12 @@ uint8_t CIO::setParameters()
   return 0U;
 }
 
-void CIO::setSoapyDeviceInfo(const std::string& type, const std::string& uri)
+void CIO::setSoapyDeviceInfo(const std::string& type, const std::string& uri, unsigned int rxGain, unsigned int txGain)
 {
   m_soapyDeviceType = type;
   m_soapyDeviceURI  = uri;
+  m_rxGain = float(rxGain);
+  m_txGain = float(txGain);
 }
 
 uint8_t CIO::setFrequency(uint8_t power, uint32_t txFreq, uint32_t rxFreq, uint32_t pocsagFreq)

IO.h
@@ -26,6 +26,7 @@
 #include <vector>
 
 #include <SoapySDR/Device.hpp>
+#include <SoapySDR/Logger.hpp>
 
 struct TXSample {
   volatile q15_t   m_sample;
@@ -55,7 +56,7 @@ public:
 
   void setMode(MMDVM_STATE state);
 
-  void setSoapyDeviceInfo(const std::string& type, const std::string& uri);
+  void setSoapyDeviceInfo(const std::string& type, const std::string& uri, unsigned int rxGain, unsigned int txGain);
   
   uint8_t setFrequency(uint8_t power, uint32_t txFreq, uint32_t rxFreq, uint32_t pocsagFreq);
   uint8_t setParameters();
@@ -108,6 +109,8 @@ private:
   uint32_t             m_txFreq;
   uint32_t             m_rxFreq;
   uint32_t             m_pocsagFreq;
+  float                m_rxGain;
+  float                m_txGain;
 
   // Frequencies as used by SoapySDR
   double               m_soapyTXFreq;

MMDVM-IQ.cpp
@@ -263,7 +263,7 @@ int CMMDVMIQ::run()
         return 1;
     }
 
-    io.setSoapyDeviceInfo(m_conf.getModemType(), m_conf.getModemURI());
+    io.setSoapyDeviceInfo(m_conf.getModemType(), m_conf.getModemURI(), m_conf.getRxGain(), m_conf.getTxGain());
 
     ret = io.start(m_conf.getModemTrace());
     if (!ret) {

MMDVM-IQ.ini
@@ -18,11 +18,20 @@ Name=modem
 [Modem]
 # For the SXceiver
 Type=sx
+RxGain=50
+TxGain=30
 # For the Pluto SDR
 # Type=plutosdr
+# RxGain=30
+# TxGain=89
 # URI=ip:pluto.local        # Soapy URI, use usb:1.1.5 for plutosdr
 # For the Lime SDR
 # Type=limesdr
+# RxGain=30
+# TxGain=60
+# Type=limemini             # or limenet-micro for the LimeNET-micro v1
+# RxGain=30
+# TxGain=64
 # URI="serial=xxxx"
 Trace=0
 